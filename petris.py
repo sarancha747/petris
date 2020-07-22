b4dab5d29e02b396de677acf6813649d9c095345
sarancha747 <68507099+sarancha747@users.noreply.github.com>
2020-07-22 21:00:25 +0300

    Update petris.py

--- petris.py
+++ petris.py
@@ -1,4 +1,6 @@
-import time, curses, threading
+import time
+import curses
+import threading
 
 E = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,6 +25,7 @@ E = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      ]
 
 my_timer = 0
+starter = 0
 
 def matrix(stdscr):
     x = 0
@@ -41,8 +44,8 @@ def matrix(stdscr):
 def o_block(stdscr):
     c_x = 0
     c_y = 0
+    f_type = 'o_block'
     while True:
-
         E[c_y][c_x] = 1
         E[c_y][c_x + 1] = 1
         E[c_y + 1][c_x + 1] = 1
@@ -54,45 +57,94 @@ def o_block(stdscr):
             break
         if my_timer == 0:
             my_timer = 1
-            countdown_thread = threading.Thread(target=countdown, args=(c_y, c_x, stdscr,))
+            countdown_thread = threading.Thread(target=countdown, args=(c_y, c_x, f_type, stdscr,))
             countdown_thread.start()
-            c_y += 1
+            if starter == 1:
+                c_y += 1
         key = stdscr.getch()
-        # if key == curses.KEY_DOWN:
-        #     E[c_y][c_x] = 0
-        #     E[c_y][c_x + 1] = 0
-        #     c_y += 1
-
-        if key == curses.KEY_RIGHT:
+        if key == curses.KEY_DOWN:
+            E[c_y][c_x] = 0
+            E[c_y][c_x + 1] = 0
+            c_y += 1
+        if key == curses.KEY_RIGHT and c_x != 8 and E[c_y][c_x + 2] != 1 and E[c_y + 1][c_x + 2] != 1:
             E[c_y][c_x] = 0
             E[c_y + 1][c_x] = 0
             c_x += 1
             stdscr.refresh()
-
-        if key == curses.KEY_LEFT:
+        if key == curses.KEY_LEFT and c_x != 0 and E[c_y][c_x - 1] != 1 and E[c_y + 1][c_x - 1] != 1:
             E[c_y][c_x + 1] = 0
             E[c_y + 1][c_x + 1] = 0
             c_x -= 1
             stdscr.refresh()
-
         stdscr.refresh()
 
     return
 
-def countdown(c_y, c_x, stdscr):
-    time.sleep(1)
-    E[c_y][c_x] = 0
-    E[c_y][c_x + 1] = 0
-    global my_timer
-    my_timer = 0
-    stdscr.refresh()
+def l_block(stdscr):
+    c_x = 0
+    c_y = 0
+    f_type = 'l_block'
+    while True:
+        E[c_y][c_x] = 1
+        E[c_y][c_x + 1] = 1
+        E[c_y][c_x + 2] = 1
+        E[c_y][c_x + 3] = 1
+        matrix(stdscr)
+        stdscr.refresh()
+        global my_timer
+        #     if c_y + 1 == 19 or E[c_y + 2][c_x] == 1 or E[c_y + 2][c_x + 1] == 1:
+        #         break
+        if my_timer == 0:
+            my_timer = 1
+            countdown_thread = threading.Thread(target=countdown, args=(c_y, c_x, f_type, stdscr,))
+            countdown_thread.start()
+            if starter == 1:
+                c_y += 1
+    #     key = stdscr.getch()
+    #     if key == curses.KEY_DOWN:
+    #          E[c_y][c_x] = 0
+    #          E[c_y][c_x + 1] = 0
+    #          c_y += 1
+    #     if key == curses.KEY_RIGHT and c_x != 8 and E[c_y][c_x + 2] != 1 and E[c_y + 1][c_x + 2] != 1:
+    #         E[c_y][c_x] = 0
+    #         E[c_y + 1][c_x] = 0
+    #         c_x += 1
+    #         stdscr.refresh()
+    #     if key == curses.KEY_LEFT and c_x != 0 and E[c_y][c_x - 1] != 1 and E[c_y + 1][c_x - 1] != 1:
+    #         E[c_y][c_x + 1] = 0
+    #         E[c_y + 1][c_x + 1] = 0
+    #         c_x -= 1
+    #         stdscr.refresh()
+    #     stdscr.refresh()
+    #
+    # return
+
+def countdown(c_y, c_x, f_type, stdscr):
+    if f_type == 'o_block':
+        E[c_y][c_x] = 0
+        E[c_y][c_x + 1] = 0
+        time.sleep(1)
+        global starter
+        starter = 1
+        global my_timer
+        my_timer = 0
+        stdscr.refresh()
+    if f_type == 'l_block':
+        E[c_y][c_x] = 0
+        E[c_y][c_x + 1] = 0
+        E[c_y][c_x + 2] = 0
+        E[c_y][c_x + 3] = 0
+        time.sleep(1)
+        starter = 1
+        my_timer = 0
+        stdscr.refresh()
 
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(1)
-
     while True:
-        o_block(stdscr)
+        # o_block(stdscr)
+        l_block(stdscr)
         # stdscr.clear()
 
 if __name__ == '__main__':
