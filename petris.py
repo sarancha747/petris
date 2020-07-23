ff7befbd0fa85206e413f7ced82cba931d5e9a7e
sarancha747 <68507099+sarancha747@users.noreply.github.com>
2020-07-23 20:49:31 +0300

    Update petris.py

--- petris.py
+++ petris.py
@@ -1,6 +1,7 @@
 import time
 import curses
 import threading
+import random
 
 E = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,9 +24,9 @@ E = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      ]
-
 my_timer = 0
 starter = 0
+
 
 def matrix(stdscr):
     x = 0
@@ -40,6 +41,7 @@ def matrix(stdscr):
                 break
             x += 1
         y += 1
+
 
 def o_block(stdscr):
     c_x = 0
@@ -80,6 +82,7 @@ def o_block(stdscr):
 
     return
 
+
 def l_block(stdscr):
     c_x = 0
     c_y = 0
@@ -92,32 +95,34 @@ def l_block(stdscr):
         matrix(stdscr)
         stdscr.refresh()
         global my_timer
-        #     if c_y + 1 == 19 or E[c_y + 2][c_x] == 1 or E[c_y + 2][c_x + 1] == 1:
-        #         break
+        if c_y == 19 or E[c_y + 1][c_x] == 1 or E[c_y + 1][c_x + 1] == 1 or E[c_y + 1][c_x + 2] == 1 or E[c_y + 1][
+            c_x + 3] == 1:
+            break
         if my_timer == 0:
             my_timer = 1
             countdown_thread = threading.Thread(target=countdown, args=(c_y, c_x, f_type, stdscr,))
             countdown_thread.start()
             if starter == 1:
                 c_y += 1
-    #     key = stdscr.getch()
-    #     if key == curses.KEY_DOWN:
-    #          E[c_y][c_x] = 0
-    #          E[c_y][c_x + 1] = 0
-    #          c_y += 1
-    #     if key == curses.KEY_RIGHT and c_x != 8 and E[c_y][c_x + 2] != 1 and E[c_y + 1][c_x + 2] != 1:
-    #         E[c_y][c_x] = 0
-    #         E[c_y + 1][c_x] = 0
-    #         c_x += 1
-    #         stdscr.refresh()
-    #     if key == curses.KEY_LEFT and c_x != 0 and E[c_y][c_x - 1] != 1 and E[c_y + 1][c_x - 1] != 1:
-    #         E[c_y][c_x + 1] = 0
-    #         E[c_y + 1][c_x + 1] = 0
-    #         c_x -= 1
-    #         stdscr.refresh()
+        key = stdscr.getch()
+        if key == curses.KEY_DOWN:
+            E[c_y][c_x] = 0
+            E[c_y][c_x + 1] = 0
+            E[c_y][c_x + 2] = 0
+            E[c_y][c_x + 3] = 0
+            c_y += 1
+        if key == curses.KEY_RIGHT and c_x != 6 and E[c_y][c_x + 4] != 1:
+            E[c_y][c_x] = 0
+            c_x += 1
+            stdscr.refresh()
+        if key == curses.KEY_LEFT and c_x != 0 and E[c_y][c_x - 1] != 1:
+            E[c_y][c_x + 3] = 0
+            c_x -= 1
+            stdscr.refresh()
     #     stdscr.refresh()
     #
     # return
+
 
 def countdown(c_y, c_x, f_type, stdscr):
     if f_type == 'o_block':
@@ -139,13 +144,15 @@ def countdown(c_y, c_x, f_type, stdscr):
         my_timer = 0
         stdscr.refresh()
 
+
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(1)
+    block_list = [o_block(stdscr), l_block(stdscr)]
+    number_list = [1, 2]
     while True:
-        # o_block(stdscr)
-        l_block(stdscr)
-        # stdscr.clear()
+        print(random.choice(block_list))  # block_list)
+
 
 if __name__ == '__main__':
     curses.wrapper(main)
